f8d705a2b8e388da598b281989654ff6a8990390
Atharva Nale <atharvnale26@gmail.com>
2025-11-02 00:01:11 +0530

    Initial commit

---
+++ build/pymoveit2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/atharva/robot_ws/src/pymoveit2
+# Build directory: /home/atharva/robot_ws/build/pymoveit2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/atharva/robot_ws/install/pymoveit2/share/pymoveit2/package.sh"
